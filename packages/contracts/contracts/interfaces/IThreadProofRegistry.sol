40c6e58106e8008f57989ef61240ade3ee1dbda3
Md. Sadman Hasan Talukder <133516078+sadmanHT@users.noreply.github.com>
2026-08-30 02:40:11 +0600

    feat(contracts): add organization registry interface

---
+++ packages/contracts/contracts/interfaces/IThreadProofRegistry.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface IThreadProofRegistry {
+    function organizationOfAccount(address account) external view returns (bytes32 organizationId);
+    function isActive(bytes32 organizationId) external view returns (bool);
+}
