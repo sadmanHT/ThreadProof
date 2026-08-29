9e225b9e2449fae1c3a31412f9ac3363eda1470e
Md. Sadman Hasan Talukder <133516078+sadmanHT@users.noreply.github.com>
2026-08-30 03:32:17 +0600

    feat(contracts): expose organization roles in registry interface

--- packages/contracts/contracts/interfaces/IThreadProofRegistry.sol
+++ packages/contracts/contracts/interfaces/IThreadProofRegistry.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.28;
 interface IThreadProofRegistry {
     function organizationOfAccount(address account) external view returns (bytes32 organizationId);
     function isActive(bytes32 organizationId) external view returns (bool);
+    function roleOf(bytes32 organizationId) external view returns (uint8);
 }
